ba6bb80b291979f191b13a270d985609dbde8a57
Michael Holtzscher <thenerdproject1@gmail.com>
2018-04-02 10:46:21 -0500

    Fix code formatting and code cleanup

--- spacy_readability/__init__.py
+++ spacy_readability/__init__.py
@@ -14,16 +14,16 @@ class Readability(object):
         Doc.set_extension('dale_chall', getter=self.dale_chall)
 
     def __call__(self, doc):
-        self.num_sents = len(list(doc.sents))
+        self.num_sentences = len(list(doc.sents))
         self.num_words = self.get_num_words(doc)
         self.num_syllables = self.get_num_syllables(doc)
         return doc
 
-    def fk_grade(self, doc):
-        return (11.8 * self.num_syllables / self.num_words) + (0.39 * self.num_words / self.num_sents) - 15.59
+    def fk_grade(self):
+        return (11.8 * self.num_syllables / self.num_words) + (0.39 * self.num_words / self.num_sentences) - 15.59
 
-    def fk_ease(self, doc):
-        return 206.835 - ((1.015 * self.num_words) / self.num_sents) - ((84.6 * self.num_syllables) / self.num_words)
+    def fk_ease(self):
+        return 206.835 - ((1.015 * self.num_words) / self.num_sentences) - ((84.6 * self.num_syllables) / self.num_words)
 
     def dale_chall(self, doc):
         diff_words_count = 0
@@ -32,7 +32,7 @@ class Readability(object):
                 diff_words_count += 1
 
         percent_difficult_words = 100 * diff_words_count / self.num_words
-        average_sentence_length = self.num_words / self.num_sents
+        average_sentence_length = self.num_words / self.num_sentences
         grade = 0.1579 * percent_difficult_words + 0.0496 * average_sentence_length
 
         if percent_difficult_words > 5:
@@ -49,12 +49,12 @@ class Readability(object):
 
     def get_num_syllables(self, doc):
         # filter spaces
-        words = (w for w in doc if not w.is_space)
+        words_ = (w for w in doc if not w.is_space)
 
         # filter punctuation
-        words = (w for w in words if not w.is_punct)
+        words_ = (w for w in words_ if not w.is_punct)
 
-        syllables_per_word = tuple(self.syllables(word) for word in words)
+        syllables_per_word = tuple(self.syllables(word) for word in words_)
 
         # set extension
         return sum(syllables_per_word)
