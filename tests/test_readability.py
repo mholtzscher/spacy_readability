74cbd7192763bafb120aebcb06828094d94a88c4
Michael Holtzscher <thenerdproject1@gmail.com>
2018-04-02 11:15:45 -0500

    Add new unit test

--- tests/test_readability.py
+++ tests/test_readability.py
@@ -3,10 +3,18 @@ import pytest
 
 from spacy_readability import Readability
 
+
 @pytest.fixture(scope='function')
 def nlp():
     return English()
 
+
 def test_simple(nlp):
     doc = nlp("sample")
     assert doc
+
+
+def test_integration(nlp):
+    read = Readability(nlp)
+    nlp.add_pipe(read, last=True)
+    assert nlp.pipe_names[-1] == 'readability'
